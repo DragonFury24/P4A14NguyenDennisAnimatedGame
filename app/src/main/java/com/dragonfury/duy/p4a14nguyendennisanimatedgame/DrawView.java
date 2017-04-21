6a0deea81e2b405fa9458f37b064a1dfb622598e
DragonFury24 <dennisdnguyen19@gmail.com>
2017-04-21 10:15:18 -0400

    Convert DrawView to SurfaceView and add SurfaceHolder object

--- app/src/main/java/com/dragonfury/duy/p4a14nguyendennisanimatedgame/DrawView.java
+++ app/src/main/java/com/dragonfury/duy/p4a14nguyendennisanimatedgame/DrawView.java
@@ -3,20 +3,38 @@ package com.dragonfury.duy.p4a14nguyendennisanimatedgame;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.view.View;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
 import android.content.Context;
 import android.graphics.Bitmap;
 
 /**
  * Created by 1383504 on 4/19/2017.
  */
-public class DrawView extends View {
+public class DrawView extends SurfaceView {
 
     private Bitmap heroBmp; // Declare space fir Bitmap called heroBmp, global scope
+    private SurfaceHolder holder; //Declares space for a SurfaceHolder called holder
     public DrawView(Context context) { // Constructor because it has the same name as the class
         super(context); //Calls View(context), Parent's constructor
-        //Instantiate heroBmp - assign to heroBmp for the first time
-        heroBmp = BitmapFactory.decodeResource(getResources(), R.drawable.bluejeans);
+        heroBmp = BitmapFactory.decodeResource(getResources(), R.drawable.bluejeans); //Instantiate heroBmp - assign to heroBmp for the first time
+        holder = getHolder();
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+            }
+        });
     }
 
     @Override
