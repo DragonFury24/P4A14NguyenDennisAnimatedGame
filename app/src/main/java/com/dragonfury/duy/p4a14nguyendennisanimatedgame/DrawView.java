2fa5a1ce7115a5301b4b28d9c778cb961aeb1b97
DragonFury24 <dennisdnguyen19@gmail.com>
2017-04-21 11:00:25 -0400

    Implemented LoopThread into DrawView

--- app/src/main/java/com/dragonfury/duy/p4a14nguyendennisanimatedgame/DrawView.java
+++ app/src/main/java/com/dragonfury/duy/p4a14nguyendennisanimatedgame/DrawView.java
@@ -15,14 +15,21 @@ public class DrawView extends SurfaceView {
 
     private Bitmap heroBmp; // Declare space fir Bitmap called heroBmp, global scope
     private SurfaceHolder holder; //Declares space for a SurfaceHolder called holder
+    private LoopThread loopThread; //Declares space for a LoopThread called loopThread
+    private int x = 0;
+
     public DrawView(Context context) { // Constructor because it has the same name as the class
         super(context); //Calls View(context), Parent's constructor
+
         heroBmp = BitmapFactory.decodeResource(getResources(), R.drawable.bluejeans); //Instantiate heroBmp - assign to heroBmp for the first time
         holder = getHolder();
+        loopThread = new LoopThread(this);
+
         holder.addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
+                loopThread.setRunning(true);
+                loopThread.start();
             }
 
             @Override
@@ -32,7 +39,15 @@ public class DrawView extends SurfaceView {
 
             @Override
             public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                boolean retry = true;
+                loopThread.setRunning(false);
+                while (retry) {
+                    try {
+                        loopThread.join();
+                    } catch (InterruptedException e) {
 
+                    }
+                }
             }
         });
     }
@@ -40,7 +55,11 @@ public class DrawView extends SurfaceView {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
         canvas.drawColor(Color.BLACK);
+        if (x < getWidth() - heroBmp.getWidth()) {
+            x++;
+        }
         canvas.drawBitmap(heroBmp, 10 * 1440 / getWidth(), 10 * 2560 / getHeight(), null); // Draw heroBmp at (10, 10)
     }
 }
