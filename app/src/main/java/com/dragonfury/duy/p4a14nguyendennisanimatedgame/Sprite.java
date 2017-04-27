7c881c112ec547fdc91c707236b6ad2fa33755a2
DragonFury24 <dennisdnguyen19@gmail.com>
2017-04-27 10:44:59 -0400

    Add walking animation using Sprite

--- app/src/main/java/com/dragonfury/duy/p4a14nguyendennisanimatedgame/Sprite.java
+++ app/src/main/java/com/dragonfury/duy/p4a14nguyendennisanimatedgame/Sprite.java
@@ -2,6 +2,7 @@ package com.dragonfury.duy.p4a14nguyendennisanimatedgame;
 
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Rect;
 import android.graphics.RectF;
 
 /**
@@ -12,10 +13,16 @@ public class Sprite extends RectF {
     public Sprite (float left, float top, float right, float bottom, Bitmap heroBMP) {
         super(left, top, right, bottom);
         this.heroBMP = heroBMP;
+        iWidth = heroBMP.getWidth() / BMP_COLS; //Calculate width of 1 icon
+        iHeight = heroBMP.getHeight() / BMP_ROWS; //Calculate height of 1 icon
     }
     public static final Creator <RectF> CREATOR = null;
     private int xSpeed = 5, ySpeed = 10; //Declare and instantiate
     Bitmap heroBMP; //Received bitmap stores instance heroBMP
+    private static final int BMP_ROWS = 4; //Number of rows on sprite sheet
+    private static int BMP_COLS = 4; //Number of columns on sprite sheet
+    private int currentFrame = 0; //Frame to be drawn
+    private int iWidth, iHeight; //Dimensions of 1 icon on sprite sheet
 
     public void update(Canvas c) {
         if (right + xSpeed >= c.getWidth()) {xSpeed *=-1;} //Invert xSpeed and ySpeed when boundaries reached (bounce off walls)
@@ -23,11 +30,16 @@ public class Sprite extends RectF {
         if (bottom + ySpeed >= c.getHeight()) {ySpeed *= -1;}
         else if (top + ySpeed <= 0) {ySpeed *= -1;}
         offset(xSpeed, ySpeed); //Increment x and y directions
+        currentFrame = ++currentFrame % BMP_COLS; //Advanced to next frame, returns to 0 when past max
     }
 
     public void draw(Canvas c) {
+        int srcX = currentFrame * iWidth; //Set x of current icon
+        int srcY = 1 * iHeight; //Set y to 2 rows of icons
+        Rect src = new Rect(srcX, srcY, srcX + iWidth, srcY + iHeight); //Define rectangle to be drawn (1 icon)
+
         update(c); //Modify Sprite
-        c.drawBitmap(heroBMP, null, this, null);
+        c.drawBitmap(heroBMP, src, this, null);
     }
 
 }
